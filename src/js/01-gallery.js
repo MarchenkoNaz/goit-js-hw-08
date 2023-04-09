47b6c76d81025a3d751f8dd5b2458b5793946f8a
Nazar Marchenko <109473164+MarchenkoNaz@users.noreply.github.com>
2023-04-09 20:52:44 +0300

    gallery made with simpleLightBox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+//!Markup
+const galleryList = document.querySelector('.gallery')
+const markup = createMarkup(galleryItems)
+
+function createMarkup(arr) {
+	return arr.map(el => {
+		return `<li class="gallery__item">
+					<a class="gallery__link" href="${el.original}">
+						<img class="gallery__image" src="${el.preview}" data-source="${el.original}" alt="${el.description}"/>
+					</a>
+				</li>`
+	}).join("")
+}
+galleryList.insertAdjacentHTML('beforeend', markup)//Insert markup in ul list in html document
+
+new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, });//Used for showing photos in large size
+
+
