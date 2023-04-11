b7fa051350f185cdf215eb2b248ca009c577bcb1
Nazar Marchenko <109473164+MarchenkoNaz@users.noreply.github.com>
2023-04-11 18:57:21 +0300

    done third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const SAVEDDATA = 'feedback-form-state'
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+
+onReload()
+const formData = {};
+
+function onFormData(evt) {
+	formData[evt.target.name] = evt.target.value;
+	localStorage.setItem(SAVEDDATA, JSON.stringify(formData));
+}
+
+
+function onSubmitForm(evt) {
+	console.log(JSON.parse(localStorage.getItem(SAVEDDATA)))
+	evt.preventDefault();
+	evt.currentTarget.reset();
+	localStorage.removeItem(SAVEDDATA)
+};
+
+function onReload() {
+	const data = JSON.parse(localStorage.getItem(SAVEDDATA));
+	const email = document.querySelector('.feedback-form input');
+	const message = document.querySelector('.feedback-form textarea');
+	if (data) {
+		email.value = data.email;
+		message.value = data.message;
+	}
+};
